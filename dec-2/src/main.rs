41e31b1e812b0d5f2db038d77836aeed3804ec01
Thomas Andolf <thomas.andolf@gmail.com>
2021-12-11 15:41:29 +0100

    added solution to dec-2 second problem

--- dec-2/src/main.rs
+++ dec-2/src/main.rs
@@ -30,7 +30,9 @@ struct Instruction {
 
 fn main() {
     let final_sum = dive_part_one("./src/input.txt");
-    println!("{}", final_sum);
+    println!("part one: {}", final_sum);
+    let final_sum = dive_part_two("./src/input.txt");
+    println!("part two: {}", final_sum);
 }
 
 fn read_lines(filename: &str) -> std::io::Result<Lines<BufReader<File>>> {
@@ -58,6 +60,39 @@ fn dive_part_one(filename: &str) -> u32 {
                 Directions::FORWARD => horizontal += instr.value,
                 Directions::DOWN => depth += instr.value,
                 Directions::UP => depth -= instr.value,
+            }
+        }
+        horizontal * depth
+    } else {
+        0
+    };
+    sum
+}
+
+fn dive_part_two(filename: &str) -> u32 {
+    let sum = if let Ok(lines) = read_lines(filename) {
+        let instructions: Vec<Instruction> = lines
+            .map(|l| l.unwrap())
+            .map(|l| {
+                let mut values = l.split_whitespace();
+                let direction = Directions::from_str(values.next().unwrap()).unwrap();
+                let value = values.next().unwrap().parse::<u32>().unwrap();
+                Instruction { direction, value }
+            })
+            .collect();
+
+        let mut horizontal = 0;
+        let mut depth = 0;
+        let mut aim = 0;
+
+        for instr in instructions {
+            match instr.direction {
+                Directions::FORWARD => {
+                    horizontal += instr.value;
+                    depth += aim * instr.value;
+                }
+                Directions::DOWN => aim += instr.value,
+                Directions::UP => aim -= instr.value,
             }
         }
         horizontal * depth
