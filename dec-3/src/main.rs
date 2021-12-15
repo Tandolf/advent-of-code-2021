ba8d77ec7f2b7a453ac4bf29498857c6b012f190
Thomas Andolf <thomas.andolf@gmail.com>
2021-12-15 23:56:02 +0100

    added solution to dec-3 second problem

--- dec-3/src/main.rs
+++ dec-3/src/main.rs
@@ -2,7 +2,36 @@ use std::fs::File;
 use std::io::{BufRead, BufReader, Lines};
 
 fn main() {
+    let _power_consumption = get_power_consumption("./src/input.txt");
+    //println!("total power consumption is: {}", power_consumption);
+
     if let Ok(lines) = read_lines("./src/input.txt") {
+        let strings: Vec<_> = lines.map(|v| v.unwrap()).collect();
+
+        let oxygen_generator_rating = get_oxygen_generator_rating(&strings, 0);
+        let oxygen_generator_rating: u32 =
+            u32::from_str_radix(&oxygen_generator_rating, 2).unwrap();
+
+        let co2_scrubber_rating = get_co2_scrubber_rating(&strings, 0);
+        let co2_scrubber_rating: u32 = u32::from_str_radix(&co2_scrubber_rating, 2).unwrap();
+
+        println!("oxygen generator rating: {}", oxygen_generator_rating);
+        println!("CO2 scrubber rating: {}", co2_scrubber_rating);
+
+        println!(
+            "Life support rating: {}",
+            oxygen_generator_rating * &co2_scrubber_rating
+        );
+    }
+}
+
+fn read_lines(filename: &str) -> std::io::Result<Lines<BufReader<File>>> {
+    let file = File::open(filename)?;
+    Ok(BufReader::new(file).lines())
+}
+
+fn get_power_consumption(filename: &str) -> u32 {
+    if let Ok(lines) = read_lines(filename) {
         let values: Vec<_> = lines.map(|x| x.unwrap()).collect();
 
         let mut gamma_rate = Vec::new();
@@ -36,11 +65,67 @@ fn main() {
         let epsilon_rate = epsilon_rate.join("");
         let epsilon_rate = u32::from_str_radix(&epsilon_rate, 2).unwrap();
 
-        println!("total power consumption is: {}", gamma_rate * epsilon_rate);
+        let sum = epsilon_rate * gamma_rate;
+        sum
+    } else {
+        0
     }
 }
 
-fn read_lines(filename: &str) -> std::io::Result<Lines<BufReader<File>>> {
-    let file = File::open(filename)?;
-    Ok(BufReader::new(file).lines())
+fn get_oxygen_generator_rating(values: &Vec<String>, mut index: usize) -> String {
+    if values.len() == 1 || values[0].chars().count() == index {
+        return values[0].clone();
+    }
+
+    let mut ones = Vec::new();
+    let mut zeros = Vec::new();
+
+    for value in values {
+        let v: Vec<_> = value.chars().collect();
+        if v[index] == '1' {
+            ones.push(value.clone());
+        } else if v[index] == '0' {
+            zeros.push(value.clone());
+        }
+    }
+
+    index += 1;
+    let mut values = if ones.len() > zeros.len() {
+        ones
+    } else if ones.len() < zeros.len() {
+        zeros
+    } else {
+        ones
+    };
+
+    get_oxygen_generator_rating(&mut values, index)
+}
+
+fn get_co2_scrubber_rating(values: &Vec<String>, mut index: usize) -> String {
+    if values.len() == 1 || values[0].chars().count() == index {
+        return values[0].clone();
+    }
+
+    let mut ones = Vec::new();
+    let mut zeros = Vec::new();
+
+    for value in values {
+        let v: Vec<_> = value.chars().collect();
+        if v[index] == '1' {
+            ones.push(value.clone());
+        } else if v[index] == '0' {
+            zeros.push(value.clone());
+        }
+    }
+
+    index += 1;
+    let mut values = if ones.len() < zeros.len() {
+        ones
+    } else if ones.len() > zeros.len() {
+        zeros
+    } else {
+        zeros
+    };
+
+    get_co2_scrubber_rating(&mut values, index)
 }
